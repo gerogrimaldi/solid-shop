1d1ab1d292edc0e9e18ac02448a6ffc921ded34b
gerogrimaldi <gerogrimaldi2@gmail.com>
2025-03-19 14:48:36 -0300

    some configurations for monorepo

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  
   app.setGlobalPrefix('/api'); //prefijo a todas las rutas del backend
   await app.listen(8000);
 }
