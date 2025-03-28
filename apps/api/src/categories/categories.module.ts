5efa24fcd78f1a594ba74db92e1baad87898de56
Ruben Ponce <rubenponce@live.com>
2025-03-28 14:39:08 -0300

    adding stock module and stock.gateway as a service in products and categories modules

--- apps/api/src/categories/categories.module.ts
+++ apps/api/src/categories/categories.module.ts
@@ -4,9 +4,16 @@ import { CategoriesController } from './categories.controller';
 import { ProductsService } from 'src/products/products.service';
 import { PrismaService } from 'prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
+import { StockGateway } from 'src/stock/stock.gateway';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService, ProductsService, PrismaService, JwtService],
+  providers: [
+    CategoriesService,
+    ProductsService,
+    PrismaService,
+    JwtService,
+    StockGateway,
+  ],
 })
 export class CategoriesModule {}
--- apps/api/src/stock/stock.gateway.ts
+++ apps/api/src/stock/stock.gateway.ts
@@ -6,9 +6,10 @@ export class StockGateway {
   @WebSocketServer()
   server: Server;
 
-  //Este método manda una alerta cuando el stock sea igual o menor a 5
+  // Este método manda una alerta cuando el stock sea igual o menor a 5
   updateStock(productId: string, newStock: number) {
     if (newStock <= 5) {
+      console.log('Emitiendo alerta de bajo stock', { productId, newStock });
       this.server.emit('lowStockAlert', { productId, newStock });
     }
   }
